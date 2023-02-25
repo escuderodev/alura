206cb2a768d4c289870f469f7f88935e4d2264d6
EDUARDO ESCUDERO <88359144+escuderodev@users.noreply.github.com>
2023-02-25 14:31:14 -0300

    att

---
+++ Java/api_rest/api/src/test/java/br/com/escuderodev/api_rest/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.escuderodev.api_rest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
